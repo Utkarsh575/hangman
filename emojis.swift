1564259699595620bf1646ad0ca1ea7723b95a66
Utkarsh575 <35625228+Utkarsh575@users.noreply.github.com>
2024-01-09 11:59:11 +0530

    Create emojis.swift

---
+++ emojis.swift
@@ -0,0 +1,35 @@
+var emojis: [Emoji] = [
+   Emoji(symbol: "😀", name: "Grinning Face",
+   description: "A typical smiley face.", usage: "happiness"),
+   Emoji(symbol: "😕", name: "Confused Face",
+   description: "A confused, puzzled face.", usage: "unsure
+   what to think; displeasure"),
+   Emoji(symbol: "😍", name: "Heart Eyes", 
+   description: "A smiley face with hearts for eyes.",
+   usage: "love of something; attractive"),
+   Emoji(symbol: "🧑‍💻", name: "Developer",
+   description: "A person working on a MacBook (probably using Xcode
+   to write iOS apps in Swift).", usage: "apps, software,
+   programming"),
+   Emoji(symbol: "🐢", name: "Turtle", description: 
+   "A cute turtle.", usage: "something slow"),
+   Emoji(symbol: "🐘", name: "Elephant", description:
+   "A gray elephant.", usage: "good memory"),
+   Emoji(symbol: "🍝", name: "Spaghetti", 
+   description: "A plate of spaghetti.", usage: "spaghetti"),
+   Emoji(symbol: "🎲", name: "Die", description: "A
+   single die.", usage: "taking a risk, chance; game"),
+   Emoji(symbol: "⛺️", name: "Tent", description: "A
+   small tent.", usage: "camping"),
+   Emoji(symbol: "📚", name: "Stack of Books",
+   description: "Three colored books stacked on each other.",
+   usage: "homework, studying"),
+   Emoji(symbol: "💔", name: "Broken Heart",
+   description: "A red, broken heart.", usage: "extreme 
+   sadness"), Emoji(symbol: "💤", name: "Snore", 
+   description:
+   "Three blue \'z\'s.", usage: "tired, sleepiness"),
+   Emoji(symbol: "🏁", name: "Checkered Flag",
+   description: "A black-and-white checkered flag.", usage:
+   "completion")
+]
